bdef76a98afcd2d74ba0f4787c7ea2dc86ef4e56
Roger Pueyo Centelles <rogerpueyo@routek.net>
2014-09-19 15:10:38 +0200

    Add support for D-Link DIR-810L

--- targets.mk
+++ targets.mk
@@ -19,7 +19,7 @@
 # Any option defined in Makefile can be overrided from here, for instance
 #  override OWRT_SVN = svn://mysvn.com/owrt
 
-HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 wpe72 dragino2 alix x86 unifi-ap vbox vmware wrtnode ath-ib
+HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 wpe72 dragino2 alix x86 unifi-ap vbox vmware dir-810l wrtnode ath-ib
 TBUILD_LIST := openwrt
 
 ifeq ($(T),ar71xx)
@@ -304,6 +304,13 @@ ifeq ($(T),wrtnode)
  ARCH:=ramips
  TBUILD:=openwrt
  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620n-wrtnode-squashfs-sysupgrade.bin
+endif 
+
+ifeq ($(T),dir-810l)
+ NAME:=D-Link-DIR-810L
+ ARCH:=ramips
+ TBUILD:=openwrt
+ IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620a-dir-810l-squashfs-sysupgrade.bin
 endif 
 
 ifeq ($(T),kvm)
