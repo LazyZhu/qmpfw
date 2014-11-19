0c26380747f45babe9bd237359c5d23192770ae9
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-11-19 13:44:20 +0100

    Add support for Ubiquiti Unifi AP-PRO devices

--- targets.mk
+++ targets.mk
@@ -19,7 +19,7 @@
 # Any option defined in Makefile can be overrided from here, for instance
 #  override OWRT_SVN = svn://mysvn.com/owrt
 
-HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix x86 unifi-ap vbox vmware dir-810l wrtnode wt3020 ar71xx-ib atheros-ib mpc85xx-ib ramips-ib x86-ib
+HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix x86 uap-pro unifi-ap vbox vmware dir-810l wrtnode wt3020 ar71xx-ib atheros-ib mpc85xx-ib ramips-ib x86-ib
 TBUILD_LIST := openwrt
 
 ifeq ($(T),ar71xx)
@@ -282,12 +282,21 @@ ifeq ($(T),dragino2)
 endif
 
 ifeq ($(T),unifi-ap)
-  NAME:=Ubiquiti-Unifi-AP
+  NAME:=Ubiquiti-UniFi-AP
   ARCH:=ar71xx
   TBUILD:=openwrt
   PROFILE:=ath-qmp-small-node
   IMAGE:=bin/$(ARCH)/openwrt-ar71xx-generic-ubnt-unifi-squashfs-factory.bin
   SYSUPGRADE:=bin/$(ARCH)/openwrt-ar71xx-generic-ubnt-unifi-squashfs-sysupgrade.bin
+endif
+
+ifeq ($(T),uap-pro)
+  NAME:=Ubiquiti-UniFi-AP-PRO
+  ARCH:=ar71xx
+  TBUILD:=openwrt
+  PROFILE:=ath-qmp-small-node
+  IMAGE:=bin/$(ARCH)/openwrt-ar71xx-generic-ubnt-uap-pro-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/openwrt-ar71xx-generic-ubnt-uap-pro-squashfs-sysupgrade.bin
 endif
 
 ifeq ($(T),vbox)
