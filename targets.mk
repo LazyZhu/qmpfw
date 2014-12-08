f93bdd7ef70f72802579602e80656ae672b46204
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-12-08 16:02:25 +0100

    Split WT3020 into 4M and 8M profiles. Update MT7620x profiles

--- targets.mk
+++ targets.mk
@@ -19,7 +19,7 @@
 # Any option defined in Makefile can be overrided from here, for instance
 #  override OWRT_SVN = svn://mysvn.com/owrt
 
-HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix x86 uap-pro unifi-ap vbox vmware dir-810l wrtnode wt3020 ar71xx-ib atheros-ib mpc85xx-ib ramips-ib x86-ib
+HW_AVAILABLE := ar71xx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw pico2 rocket rs rspro tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-mr3040-cam tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix x86 uap-pro unifi-ap vbox vmware dir-810l wrtnode wt3020-4m wt3020-8m ar71xx-ib atheros-ib mpc85xx-ib ramips-ib x86-ib
 TBUILD_LIST := openwrt
 
 ifeq ($(T),ar71xx)
@@ -329,16 +329,25 @@ ifeq ($(T),wrtnode)
   ARCH:=ramips
   TBUILD:=openwrt
   PROFILE:=mt7620-qmp-small-node
-  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620n-wrtnode-squashfs-sysupgrade.bin
+  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620-wrtnode-squashfs-sysupgrade.bin
 endif 
 
-ifeq ($(T),wt3020)
-  NAME:=Nexx-WT3020
+ifeq ($(T),wt3020-4m)
+  NAME:=Nexx-WT3020-4M
   ARCH:=ramips
   TBUILD:=openwrt
   PROFILE:=mt7620-qmp-tiny-node
-  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620n-wt3020-4M-squashfs-factory.bin
-  SYSUPGRADE:=bin/$(ARCH)/openwrt-ramips-mt7620n-wt3020-4M-squashfs-sysupgrade.bin
+  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620-wt3020-4M-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/openwrt-ramips-mt7620-wt3020-4M-squashfs-sysupgrade.bin
+endif
+
+ifeq ($(T),wt3020-8m)
+  NAME:=Nexx-WT3020-8M
+  ARCH:=ramips
+  TBUILD:=openwrt
+  PROFILE:=mt7620-qmp-small-node
+  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620-wt3020-8M-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/openwrt-ramips-mt7620-wt3020-8M-squashfs-sysupgrade.bin
 endif
 
 ifeq ($(T),dir-810l)
@@ -346,7 +355,7 @@ ifeq ($(T),dir-810l)
   ARCH:=ramips
   TBUILD:=openwrt
   PROFILE:=mt7620-qmp-small-node
-  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620a-dir-810l-squashfs-sysupgrade.bin
+  IMAGE:=bin/$(ARCH)/openwrt-ramips-mt7620-dir-810l-squashfs-sysupgrade.bin
 endif 
 
 ifeq ($(T),kvm)
